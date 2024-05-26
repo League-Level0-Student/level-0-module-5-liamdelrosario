c578925d4df360f8f4bf4e562615446e310ba9cc
league <league@192.168.1.82>
2024-05-26 11:29:19 -0700

    qwertyuiopasdfghjklzxcvbnm

---
+++ src/_01_algorithms/_1_prime_or_not/PrimeChecker.java
@@ -0,0 +1,14 @@
+package _01_algorithms._1_prime_or_not;
+
+public class PrimeChecker {
+
+	public PrimeChecker() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/_01_algorithms/_1_prime_or_not/primeHUHUHUHUHUprimeHUHUHUHUHUUHAAJAHAHHAHAHHHAHHhhprimeHUHUHUHUHUUHAAJAHAHHAHAHHHAHHhh.java
@@ -0,0 +1,27 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class primeHUHUHUHUHUprimeHUHUHUHUHUUHAAJAHAHHAHAHHHAHHhhprimeHUHUHUHUHUUHAAJAHAHHAHAHHHAHHhh {
+
+	public primeHUHUHUHUHUprimeHUHUHUHUHUUHAAJAHAHHAHAHHHAHHhhprimeHUHUHUHUHUUHAAJAHAHHAHAHHHAHHhh() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String input =JOptionPane.showInputDialog(null, "Give me any number you want any thing possible");
+				int i=0;
+				Integer.parseInt(input);
+				for(i=0;i<17;i++) {
+					if(input % i == 0) {
+					JOptionPane.showMessageDialog(null, "it is prime");
+				}
+				}
+				
+			
+				
+			
+	}
+
+}
